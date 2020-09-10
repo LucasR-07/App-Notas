fd08740c9a4618986698d3b74c2ff31e50dfb311
LucasR-07 <lucasromero2705@gmail.com>
2020-09-10 17:06:49 -0300

    rest API terminada

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,9 +1,22 @@
 const userCtrl = {};
 
-userCtrl.getUsers = (req, res) => res.send("Users Routes");
+const User = require("../models/User");
 
-userCtrl.createUser = (req, res) => res.send("User Creted");
+userCtrl.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
 
-userCtrl.deleteUser = (req, res) => res.send("User Eliminated");
+userCtrl.createUser = async (req, res) => {
+  const { username } = req.body;
+  const newUser = new User({ username });
+  await newUser.save();
+  res.json("User Creted");
+};
+
+userCtrl.deleteUser = async (req, res) => {
+    await User.findByIdAndRemove(req.params.id)
+    res.send("User Eliminated");
+};
 
 module.exports = userCtrl;
